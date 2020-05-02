95dc1ed8ee74e36af88262626a51b46a9bd49313
shortcipher3 <chris.hall@aggiemail.usu.edu>
2020-05-02 15:00:47 -0600

    Fleshed out face detection model

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ python3 downloader.py --name gaze-estimation-adas-0002 --precisions=INT8,FP16,FP
 ## Demo
 *TODO:* Explain how to run a basic demo of your model.
 
+python3 src/face_detection.py --input=data/image_100.png --detection=models/intel/face-detection-adas-binary-0001/FP32-INT1/face-detection-adas-binary-0001 --device=CPU --log-level=info
+
 ## Documentation
 *TODO:* Include any documentation that users might need to better understand your project code. For instance, this is a good place to explain the command line arguments that your project supports.
 
@@ -23,7 +25,15 @@ python3 downloader.py --name gaze-estimation-adas-0002 --precisions=INT8,FP16,FP
 *TODO:* Discuss the benchmark results and explain why you are getting the results you are getting. For instance, explain why there is difference in inference time for FP32, FP16 and INT8 models.
 
 ## Stand Out Suggestions
-This is where you can provide information about the stand out suggestions that you have attempted.
+### Provided Stand Out Suggestions
+* Can you improve your inference speed without significant drop in performance by changing the precision of some of the models? In your README, include a short write-up that explains the procedure and the experiments you ran to find out the best combination of precision.
+* Benchmark the running times of different parts of the preprocessing and inference pipeline and let the user specify a CLI argument if they want to see the benchmark timing. Use the `get_perf_counts`  API to print the time it takes for each layer in the model.
+* Use the VTune Amplifier to find hotspots in your Inference Engine Pipeline. Write a short write-up in the README about the hotspots in your system and how you solved them.
+* There will be certain edge cases that will cause your system to not function properly. Examples of this include: lighting changes, multiple people in the same input frame, and so on. Make changes in your preprocessing and inference pipeline to solve some of these issues. Write a short write-up in the README about the problem it caused and your solution.
+* Add a toggle to the UI to shut off the camera feed and show stats only (as well as to toggle the camera feed back on). Show how this affects performance and power as a short write up in the README file.
+* Build an inference pipeline for both video file and webcam feed as input. Allow the user to select their input option in the command line arguments.
+
+### Achieved Stand Out Suggestions
 
 ### Async Inference
 If you have used Async Inference in your code, benchmark the results and explain its effects on power and performance of your project.
--- src/face_detection.py
+++ src/face_detection.py
@@ -3,26 +3,30 @@ This is a sample class for a model. You may choose to use it as-is or make any c
 This has been provided just to give you an idea of how to structure your model class.
 '''
 import time
+import logging
+from typing import Any, Dict, List
+import numpy as np
 import cv2
 from openvino.inference_engine import IECore
+
 
 class FaceDetector:
     '''
     Class for the Face Detection Model.
     '''
-    def __init__(self, model_name, device='CPU', extensions=None):
+    def __init__(self, model_name: str, device: str='CPU'):
         self.model = model_name
         self.device = device
         model_weights = self.model + '.bin'
         model_structure = self.model + '.xml'
 
         ### Load model ###
-        #t1 = cv2.getTickCount()
+        t1 = cv2.getTickCount()
         core = IECore()
         self.net = core.read_network(model=model_structure, weights=model_weights)
         self.exec_net = core.load_network(network=self.net, device_name=self.device)
-        #t2 = cv2.getTickCount()
-        #print(f'Time taken to load model = {(t2-t1)/cv2.getTickFrequency()} seconds')
+        t2 = cv2.getTickCount()
+        logging.info(f'Time taken to load face detection model = {(t2-t1)/cv2.getTickFrequency()} seconds')
 
         ### Check model ###
         # Get the supported layers of the network
@@ -32,15 +36,15 @@ class FaceDetector:
         # know if anything is missing. Exit the program, if so.
         unsupported_layers = [l for l in self.net.layers.keys() if l not in supported_layers]
         if len(unsupported_layers) != 0:
-            print("Unsupported layers found: {}".format(unsupported_layers))
-            print("Check whether extensions are available to add to IECore.")
+            logging.error("Unsupported layers found in face detection model: {}".format(unsupported_layers))
+            logging.error("Check whether extensions are available to add to IECore.")
             exit(1)
 
         # Get the input layer
         self.input_blob = next(iter(self.exec_net.inputs))
         self.output_blob = next(iter(self.exec_net.outputs))
 
-    def async_detect(self, batch):
+    def async_detect(self, batch: np.ndarray):
         """
         Asynchronously run prediction on a batch with the network
 
@@ -61,7 +65,7 @@ class FaceDetector:
 
         Returns
         -------
-            detections: the network's detections
+            raw_detections: the network's detections
         """
         while True:
             status = infer_request_handle.wait(-1)
@@ -69,10 +73,10 @@ class FaceDetector:
                 break
             else:
                 time.sleep(1)
-        detections = infer_request_handle.outputs
-        return detections
+        raw_detections = infer_request_handle.outputs
+        return raw_detections
 
-    def sync_detect(self, image):
+    def sync_detect(self, batch: np.ndarray) -> Dict[str, np.ndarray]:
         """
         Synchronously run prediction on a batch with the network
 
@@ -84,42 +88,119 @@ class FaceDetector:
         -------
             detections_arr: the array of detections
         """
-        #t1 = cv2.getTickCount()
-        detections = self.exec_net.infer({self.input_blob: image})
-        #t2 = cv2.getTickCount()
-        #print(f'Time taken to execute model = {(t2-t1)/cv2.getTickFrequency()} seconds')
+        t1 = cv2.getTickCount()
+        detections = self.exec_net.infer({self.input_blob: batch})
+        t2 = cv2.getTickCount()
+        logging.info(f'Time taken to execute face detection model model = {(t2-t1)/cv2.getTickFrequency()} seconds')
         return detections
 
-    def preprocess_input(self, image):
-        '''
+    def preprocess_input(self, image: np.ndarray, width: int=672, height: int=384, preserve_aspect_ratio: bool=False):
+        """
         Before feeding the data into the model for inference,
         you might have to preprocess it. This function is where you can do that.
-        '''
-        raise NotImplementedError
 
-    def preprocess_output(self, outputs):
-        '''
-        Before feeding the output of this model to the next model,
-        you might have to preprocess the output. This function is where you can do that.
-        '''
-        raise NotImplementedError
+        Default model takes an input of 1x3x384x672 bgr BxCxHxW
 
-    def draw_bboxes(image, detections):
+        Parameters
+        ----------
+            image: image to run preprocessing on
+            width: desired width
+            height: desired height
+            preserve_aspect_ratio: boolean, https://docs.openvinotoolkit.org/latest/_docs_MO_DG_prepare_model_convert_model_tf_specific_Convert_Object_Detection_API_Models.html specifies for different models
+
+        Returns
+        -------
+            batch: with preprocessing applied
+            normalization_consts: ratio of the image pixels to image with padding
+        """
+        normalization_consts = [1.0, 1.0]
+        if preserve_aspect_ratio:
+            rows, cols, _ = image.shape
+            fx = height * 1.0 / cols
+            fy = width * 1.0 / rows
+            if fx < fy:
+                fy = fx
+            else:
+                fx = fy
+            resized = cv2.resize(image.copy(), (0, 0), fx=fx, fy=fy)
+            batch = np.zeros((height, width, 3), np.uint8)
+            normalization_consts = [resized.shape[1] * 1.0 / batch.shape[1],
+                                    resized.shape[0] * 1.0 / batch.shape[0]]
+            batch[:resized.shape[0], :resized.shape[1], :] = resized
+        else:
+            batch = cv2.resize(image.copy(), (width, height))
+        batch = batch.transpose((2,0,1)) # Channels first
+        return batch[np.newaxis, :, :, :], normalization_consts
+
+    def preprocess_output(self, raw_detections: np.ndarray, threshold: float=0.3, whitelist_filter: List[int]=[1], normalization_consts: List[float]=[1.0, 1.0]) -> List[Dict[str, Any]]:
+        """
+        Change the format of the detections for use by the next model.
+
+        Default model produces raw_detections, a dictionary with an output array that is 1x1xNx7,
+        where each row is a detection and consists of:
+        [image_id, label, conf, x_min, y_min, x_max, y_max]
+
+        Parameters
+        ----------
+            raw_detections: the face detection network output as produced by OpenVINO, an array of detections
+            threshold: discard detections with a score lower than this threshold
+            whitelist_filter: the class ids to include, if empty it includes all of them
+
+        Returns
+        -------
+            detections: a list of detections meeting the criteria
+        """
+        logging.info(f'raw detections {raw_detections}')
+        arr = np.concatenate(raw_detections['detection_out'][:, 0, :, :], axis=0)
+        # filter based on threshold
+        arr = arr[arr[:, 2]>threshold, :]
+        #logging.info(arr) #TODO bbox output looks wrong for batch size > 1
+        #logging.info(arr.shape)
+        if whitelist_filter:
+            arr = arr[np.isin(arr[:, 1], whitelist_filter), :]
+        num_detections = arr.shape[0]
+        detections = []
+        for k in range(num_detections):
+            x_min = arr[k, 3] / normalization_consts[0]
+            x_max = arr[k, 5] / normalization_consts[0]
+            y_min = arr[k, 4] / normalization_consts[1]
+            y_max = arr[k, 6] / normalization_consts[1]
+            width = np.abs(x_max - x_min)
+            height = np.abs(y_max - y_min)
+            area = width * height
+            detections.append({'image_id': arr[k, 0],
+                               'label': arr[k, 1],
+                               'conf': arr[k, 2],
+                               'x_min': x_min,
+                               'y_min': y_min,
+                               'x_max': x_max,
+                               'y_max': y_max,
+                               'width': width,
+                               'height': height,
+                               'area': area})
+        logging.info(f'detections {detections}')
+        return detections
+
+    def visualize_detections(self, image: np.ndarray, detections: List[Dict[str, Any]]) -> np.ndarray:
         img = image.copy()
-        for i in range(detections['batch'].shape[0]):
-            #classId = int(detections['class'][i])
-            score = float(detections['score'][i])
-            bbox = [float(v) for v in detections['bbox'][i]]
-            if score > 0.3:
-                #print(f"batch: {detections['batch'][i]} class: {classId}, score: {score}, bbox: {bbox}")
-                y = bbox[1] * img.shape[0]
-                x = bbox[0] * img.shape[1]
-                bottom = bbox[3] * img.shape[0]
-                right = bbox[2] * img.shape[1]
-                cv2.rectangle(img, (int(x), int(y)), (int(right), int(bottom)), (125, 255, 51), thickness=2)
+        for det in detections:
+            x_min = int(det['x_min'] * img.shape[1])
+            x_max = int(det['x_max'] * img.shape[1])
+            y_min = int(det['y_min'] * img.shape[0])
+            y_max = int(det['y_max'] * img.shape[0])
+            cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (125, 255, 51), thickness=2)
+            cv2.putText(img,
+                     f'score: {det["conf"]:.2f} label: {det["label"]}',
+                     (x_min, y_min),
+                     cv2.FONT_HERSHEY_SIMPLEX,
+                     .5,
+                     (0,0,255),
+                     2,
+                     cv2.LINE_AA)
         return img
 
 if __name__ == '__main__':
+    # parse input arguments
     import argparse
     parser = argparse.ArgumentParser(description='Use computer vision to control your mouse position with your gaze')
     parser.add_argument('--input',
@@ -133,9 +214,32 @@ if __name__ == '__main__':
     parser.add_argument('--device',
                         default='CPU',
                         type=str,
+                        choices=['CPU', 'GPU', 'MYRIAD', 'FPGA'],
                         help='the device to run inference on, one of CPU, GPU, MYRIAD, FPGA')
+    parser.add_argument('--log-level',
+                        default='error',
+                        type=str,
+                        choices=['debug', 'info', 'warning', 'error', 'critical'],
+                        help='the log level, one of debug, info, warning, error, critical')
 
     args = parser.parse_args()
 
+    # set log level
+    LEVELS = {'debug': logging.DEBUG,
+              'info': logging.INFO,
+              'warning': logging.WARNING,
+              'error': logging.ERROR,
+              'critical': logging.CRITICAL}
+    log_level = LEVELS.get(args.log_level, logging.ERROR)
+    logging.basicConfig(level=log_level)
+
+    # run the detection network and save the output
+    image = cv2.imread(args.input)
     face_det = FaceDetector(args.detection, args.device)
+    batch, _ = face_det.preprocess_input(image)
+    dets = face_det.sync_detect(batch)
+    detections = face_det.preprocess_output(dets)
+    img = face_det.visualize_detections(image, detections)
+    cv2.imwrite('face_det.png', img)
+
 
